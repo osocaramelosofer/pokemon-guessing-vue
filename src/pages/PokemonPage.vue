6efeecf503f589a7de3f9509ffd8472cdcf6a8ef
osocaramelosofer <osocaramelosofer@hotmail.com>
2023-03-13 19:44:42 -0600

    pokemons names

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>quien es este pokemon?</h1>
+    <PokemonPicture :pokemonId="4" :showPokemon="false"/>
+    <PokemonOptions />
+  </div>
+</template>
+
+<script>
+import PokemonOptions from "@/components/PokemonOptions.vue";
+import PokemonPicture from "@/components/PokemonPicture.vue";
+
+import getPokemonOpions from "@/helpers/getPokemonOptions"
+
+console.log(getPokemonOpions())
+
+export default {
+  components: {
+    PokemonOptions,
+    PokemonPicture,
+  },
+}
+</script>
+
+<style>
+
+</style>
